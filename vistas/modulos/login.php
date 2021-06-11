c117e88b3a71b0ca2d3c650f2a31d0b25e42f17e
jim-ux <74369837@continental.edu.pe>
2021-06-11 00:00:55 -0500

    Update 00:00

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -1,53 +1,71 @@
 <div id="bak"></div>
-
-<div class="login-box">
-  
-  <div class="login-logo">
-
-    <img src="vistas/img/plantilla/ucci.jpg" class="img-responsive">
-
-  </div>
-
-  <div class="login-box-body">
-
-    <p class="login-box-msg">Ingresar al sistema</p>
-
-    <form method="post">
-
-      <div class="form-group has-feedback">
-
-        <input type="text" class="form-control" placeholder="Usuario" name="ingUsuario" required>
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-
-      </div>
-
-      <div class="form-group has-feedback">
-
-        <input type="password" class="form-control" placeholder="Contraseña" name="ingPassword" required>
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      
-      </div>
-
-      <div class="row">
-       
-        <div class="col-xs-4">
-
-          <button type="submit" class="btn btn-success btn-block btn-flat">Ingresar</button>
-          
+<br><br><br><br><br>
+<div class="container mt-5">
+<div class="row">
+      <div class="col-md-7">
         
+
+        <div class="login-logo">
+
+        <img src="vistas/img/plantilla/tienda.png" class="img-responsive">
+
         </div>
 
+
       </div>
+      <div class="col-md-5">
+      
+        <div class="container-fluid bg-success padding-bottom-5">
+            <form method="post">
 
-      <?php
-        
-        $login = new ControladorUsuarios();
-        $login -> ctrIngresoUsuario();
-        
-      ?>
+            <h3 class="">Ingreso de Usuario</h3>
 
-    </form>
+            <div class="form-group has-feedback">
 
-  </div>
+              <input type="text" class="form-control" placeholder="Usuario" name="ingUsuario" required>
+              <span class="glyphicon glyphicon-user form-control-feedback"></span>
 
-</div>
+            </div>
+
+            <div class="form-group has-feedback">
+
+              <input type="password" class="form-control" placeholder="Contraseña" name="ingPassword" required>
+              <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+
+            </div>
+
+            <div class="row">
+
+              <div class="col-xs-4">
+
+                <button type="submit" class="btn btn-success btn-block btn-flat">Ingresar</button>
+                
+              
+              </div>
+
+            </div>
+
+
+            </div>
+
+            <?php
+              
+              $login = new ControladorUsuarios();
+              $login -> ctrIngresoUsuario();
+              
+            ?>
+
+            </form>
+        </div>
+      </div>
+</div> 
+
+
+
+    
+
+    
+
+  
+
+
