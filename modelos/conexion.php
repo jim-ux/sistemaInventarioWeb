439623ac4691211f7fc5073de2f1d5a0b3c2152c
jim-ux <74369837@continental.edu.pe>
2021-06-25 15:26:16 -0500

    cambio conexion host

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=iv",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=bb2srhowikj9bbwnflop-mysql.services.clever-cloud.com;dbname=bb2srhowikj9bbwnflop",
+			            "uldjba78rsx1ypk7",
+			            "rg5HftPwvT5t9dFDPiTL");
 
 		$link->exec("set names utf8");
 
